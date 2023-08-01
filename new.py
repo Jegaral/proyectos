3d6b0b2a8057fc58d4263703c34f5d935584ba38
Kevin Garcia <kjesusg@gmail.com>
2023-07-31 21:09:48 -0600

    agrego proyecto

---
+++ new.py
@@ -0,0 +1,21 @@
+# Entero (int)
+mi_entero = 10
+# Flotante (float)
+mi_flotante = (3.14)
+# Cadena (str)
+mi_cadena = "Hola, soy una cadena"
+# Booleano (bool)
+mi_booleano = True
+
+# Definir el número 'n' hasta el cual queremos sumar los números pares
+n = 10
+# Inicializar una variable para almacenar el resultado de la suma
+resultado = 0
+# Bucle para sumar los números pares hasta 'n'
+for i in range(2, n+1, 2):
+    resultado += i
+print("La suma de los primeros números pares hasta", n, "es:", resultado)
+print(mi_entero)
+print(mi_flotante)
+print(mi_cadena)
+print(mi_booleano)
